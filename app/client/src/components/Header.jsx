6feda8caf640a52a7c4984b05402a05bc751d989
vishnaka-dasanayaka <vishnakadasanayaka@gmail.com>
2024-07-17 23:47:42 +0530

    header bar

--- app/client/src/components/Header.jsx
+++ app/client/src/components/Header.jsx
@@ -1,19 +1,29 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 function Header() {
   return (
-    <div className="flex items-center justify-center">
-      <Link to={"/"}>
-        <button className="mx-5 bg-purple-400 px-5 py-1 cursor-pointer hover:bg-purple-600">
+    <div className="flex items-center justify-between h-24">
+      <div className="flex items-center justify-center w-40 h-full ml-10 mr-5 md:ml-16 md:w-60">
+        <img src={logo} className="w-full h-fit" alt="" />
+      </div>
+      <div className="md:hidden"></div>
+      <div className="items-center justify-center hidden h-full mr-10 md:flex">
+        <h1 className="px-2 pb-2 mx-5 font-bold capitalize border-black cursor-pointer hover:border-b-2">
           home
-        </button>
-      </Link>
-      <Link to={"/about"}>
-        <button className="mx-5 bg-purple-400 px-5 py-1 cursor-pointer hover:bg-purple-600">
-          about
-        </button>{" "}
-      </Link>
+        </h1>
+        <h1 className="px-2 pb-2 mx-5 font-bold capitalize border-black cursor-pointer hover:border-b-2">
+          our partners
+        </h1>
+        <h1 className="px-2 pb-2 mx-5 font-bold capitalize border-black cursor-pointer hover:border-b-2">
+          road map
+        </h1>
+        <h1 className="px-2 pb-2 mx-5 font-bold capitalize border-black cursor-pointer hover:border-b-2">
+          contact us
+        </h1>
+        <h1 className="px-2 pb-2 mx-5 font-bold capitalize border-black cursor-pointer hover:border-b-2">
+          about us
+        </h1>
+      </div>
     </div>
   );
 }
--- app/client/src/components/Hero.jsx
+++ app/client/src/components/Hero.jsx
@@ -4,24 +4,24 @@ import HeroImage from "../assets/hero_image.png";
 const Hero = () => {
   return (
     <section className="bg-[#fcf7f0]">
-      <div className="flex flex-col md:flex-row lg:flex-row pt-16 pl-12 lg:pl-28 pr-10 pb-12 items-center justify-between ">
+      <div className="flex flex-col items-center justify-between pb-12 pl-12 pr-10 md:flex-row lg:flex-row lg:pl-28 ">
         <div>
-          <h1 className="text-4xl md:text-4xl lg:text-6xl font-Montserrat text-center md:text-left lg:text-left">
+          <h1 className="text-4xl text-center md:text-4xl lg:text-6xl font-Montserrat md:text-left lg:text-left">
             RUH-EIE
           </h1>
-          <h1 className="text-5xl md:text-5xl lg:text-7xl font-Montserrat font-bold  text-center md:text-left lg:text-left">
+          <h1 className="text-5xl font-bold text-center md:text-5xl lg:text-7xl font-Montserrat md:text-left lg:text-left">
             CAREER FAIR
           </h1>
           <h1 className="text-3xl md:text-3xl lg:text-5xl drop-shadow-xl font-Montserrat text-[#FF9503] text-center md:text-left lg:text-left">
             2024
           </h1>
-          <p className="max-w-2xl mb-6 pt-3 font-Montserrat text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-black text-center md:text-left lg:text-left">
+          <p className="max-w-2xl pt-3 mb-6 text-center text-gray-500 font-Montserrat lg:mb-8 md:text-lg lg:text-xl dark:text-black md:text-left lg:text-left">
             Explore thousands of job opportunities with all the information you
             need.It's your future. Come find it. Manage all your job
             applications from start to finish
           </p>
-          <div className="flex pb-5 justify-center  md:justify-start lg:justify-start">
-            <button className="bg-black text-white text-base md:text-base lg:text-lg font-Montserrat h-10 w-36 rounded-lg">
+          <div className="flex justify-center pb-5 md:justify-start lg:justify-start">
+            <button className="h-10 text-base text-white bg-black rounded-lg md:text-base lg:text-lg font-Montserrat w-36">
               Find Your Job
             </button>
           </div>
@@ -31,7 +31,7 @@ const Hero = () => {
           <img
             src={HeroImage}
             alt="Logo"
-            className="relative w-full h-auto rounded-lg pl-8"
+            className="relative w-full h-auto pl-8 rounded-lg"
           />
         </div>
       </div>
